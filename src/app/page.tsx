787249965dac2e6b476c2ef700f19b6fa7b015f6
islem-habta <167141087+islem-habta@users.noreply.github.com>
2025-03-06 13:34:54 +0100

    Removed .env file and secrets from history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen">
+      <Link
+        href="/admin"
+        className="px-6 py-3 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition"
+      >
+        Admin Page
+      </Link>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
